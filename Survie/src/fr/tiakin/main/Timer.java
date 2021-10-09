fa95607c9327230862301d6621020add125420b8
Tiakin <killian.duboucher@outlook.fr>
2021-10-09 22:03:45 +0200

    fix

--- Survie/src/fr/tiakin/main/Timer.java
+++ Survie/src/fr/tiakin/main/Timer.java
@@ -1,6 +1,7 @@
 package fr.tiakin.main;
 
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -14,28 +15,29 @@ public class Timer extends BukkitRunnable {
 	@Override
 	public void run() {
 		for(Player p : Bukkit.getOnlinePlayers()) {
-			if(Custom.hasShield(p)) {
-				int totalShield = Custom.getTotalShield(p);
-				float shield = Custom.getShield(p);
-				if(totalShield > 0)
-					p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(Custom.actionShield(shield, totalShield)));
-				if(shield < totalShield) {
-					if(damageCooldown.getPlayerTiming(p) > 5000l)
-						if(healCooldown.isPlayerOnTiming(p)) {
-							if(healCooldown.getPlayerTiming(p) > 1000l) {
-								Custom.setShield(p, shield+1);
+			if(p.getGameMode().equals(GameMode.SURVIVAL) || p.getGameMode().equals(GameMode.SPECTATOR))
+				if(Custom.hasShield(p)) {
+					int totalShield = Custom.getTotalShield(p);
+					float shield = Custom.getShield(p);
+					if(totalShield > 0)
+						p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(Custom.actionShield(shield, totalShield)));
+					if(shield < totalShield) {
+						if(damageCooldown.getPlayerTiming(p) > 5000l)
+							if(healCooldown.isPlayerOnTiming(p)) {
+								if(healCooldown.getPlayerTiming(p) > 1000l) {
+									Custom.setShield(p, shield+1);
+									healCooldown.addPlayerOnTiming(p);
+								}
+							}else {
 								healCooldown.addPlayerOnTiming(p);
 							}
-						}else {
-							healCooldown.addPlayerOnTiming(p);
-						}
+					}
+					if(shield > totalShield) {
+						Custom.setShield(p, totalShield);
+					}
+				}else {
+					Custom.setShield(p, 0);
 				}
-				if(shield > totalShield) {
-					Custom.setShield(p, totalShield);
-				}
-			}else {
-				Custom.setShield(p, 0);
-			}
 		}
 	}
 

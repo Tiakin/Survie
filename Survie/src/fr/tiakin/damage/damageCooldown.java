550363b5cad7219ca3a875983fa912902d288b84
Tiakin <killian.duboucher@outlook.fr>
2021-10-09 23:10:01 +0200

    ez

--- Survie/src/fr/tiakin/damage/damageCooldown.java
+++ Survie/src/fr/tiakin/damage/damageCooldown.java
@@ -2,14 +2,10 @@ package fr.tiakin.damage;
 
 import java.util.HashMap;
 import org.bukkit.entity.Player;
-import org.bukkit.plugin.java.JavaPlugin;
-
-import fr.tiakin.main.main;
 
 public class damageCooldown {
 	private static HashMap<Player,Long> timelist = new HashMap<>();
 	public static void addPlayerOnTiming(Player p){
-    	JavaPlugin.getPlugin(main.class).getLogger().info("cooldown :x");
     	if(timelist.containsKey(p))
     		timelist.replace(p, System.currentTimeMillis());
     	else
--- Survie/src/fr/tiakin/damage/damageEvent.java
+++ Survie/src/fr/tiakin/damage/damageEvent.java
@@ -4,26 +4,31 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import fr.tiakin.main.Custom;
+import fr.tiakin.main.main;
 
 public class damageEvent implements Listener {
 	
 	@EventHandler
     public void damage(EntityDamageEvent e) {
 		if(e.getEntity() instanceof Player) {
-			System.out.println("ez");
 			Player p = (Player) e.getEntity();
 			damageCooldown.addPlayerOnTiming(p);
-			float rest = (float) (Custom.getShield(p) - (e.getDamage() / 10) );
+			float rest = (float) (Custom.getShield(p) - (e.getDamage() / 2.5) );
+			JavaPlugin.getPlugin(main.class).getLogger().info(rest+"");
 			if(rest < 0) {
 				Custom.setShield(p, 0);
-				rest *= 10;
+				rest *= 2.5;
+				rest = Math.abs(rest);
 			}else {
 				Custom.setShield(p, rest);
 				rest = 0;
 			}
-			e.setDamage(rest*10);
+			JavaPlugin.getPlugin(main.class).getLogger().info(rest+"");
+			e.setDamage(rest);
+			JavaPlugin.getPlugin(main.class).getLogger().info(e.getFinalDamage()+"");
 		}
 	}
 }

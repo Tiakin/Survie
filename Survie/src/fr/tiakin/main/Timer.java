0f7b77e29c8e59e66d5bae4594e0dd662fb3f114
Tiakin <killian.duboucher@outlook.fr>
2021-10-09 21:32:35 +0200

    fix !

--- Survie/src/fr/tiakin/main/Timer.java
+++ Survie/src/fr/tiakin/main/Timer.java
@@ -7,22 +7,34 @@ import org.bukkit.scheduler.BukkitRunnable;
 import net.md_5.bungee.api.ChatMessageType;
 import net.md_5.bungee.api.chat.TextComponent;
 import fr.tiakin.damage.damageCooldown;
+import fr.tiakin.damage.healCooldown;
 
 public class Timer extends BukkitRunnable {
 	
 	@Override
 	public void run() {
 		for(Player p : Bukkit.getOnlinePlayers()) {
-			int totalShield = Custom.getTotalShield(p);
-			float shield = Custom.getShield(p);
-			if(totalShield > 0)
-				p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(Custom.actionShield(shield, totalShield)));
-			if(shield < totalShield) {
-				if(damageCooldown.getPlayerTiming(p) > 2000l)
-					Custom.setShield(p, shield+1);
-			}
-			if(shield > totalShield) {
-				Custom.setShield(p, totalShield);
+			if(Custom.hasShield(p)) {
+				int totalShield = Custom.getTotalShield(p);
+				float shield = Custom.getShield(p);
+				if(totalShield > 0)
+					p.spigot().sendMessage(ChatMessageType.ACTION_BAR, new TextComponent(Custom.actionShield(shield, totalShield)));
+				if(shield < totalShield) {
+					if(damageCooldown.getPlayerTiming(p) > 5000l)
+						if(healCooldown.isPlayerOnTiming(p)) {
+							if(healCooldown.getPlayerTiming(p) < 1000l) {
+								Custom.setShield(p, shield+1);
+								healCooldown.addPlayerOnTiming(p);
+							}
+						}else {
+							healCooldown.addPlayerOnTiming(p);
+						}
+				}
+				if(shield > totalShield) {
+					Custom.setShield(p, totalShield);
+				}
+			}else {
+				Custom.setShield(p, 0);
 			}
 		}
 	}
